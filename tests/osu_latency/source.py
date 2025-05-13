d71a0bd2faae334fad80a6901ba757a559daa702
pereira-fabio <pereira.fabio@hotmail.fr>
2025-05-14 00:56:08 +0200

    source

--- tests/osu_latency/source.py
+++ tests/osu_latency/source.py
@@ -4,39 +4,18 @@ import reframe.utility.sanity as sn
 @rfm.simple_test
 class OSULatencyTest(rfm.RegressionTest):
     def __init__(self):
-        # Test metadata
-        self.descr = 'OSU Latency Test (pre-installed)'
-        self.valid_systems = ['aion_standalone:default ', 'aion:cpu']
+        # Remove all spaces in system names
+        self.valid_systems = ['aion_standalone:default', 'aion:cpu']
         self.valid_prog_environs = ['builtin']
         
-        # Using pre-installed binary
+        # Rest of your test configuration...
+        self.descr = 'OSU Latency Test'
         self.executable = 'osu_latency'
-        self.executable_opts = ['-x', '100', '-i', '1000']  # Warmup and iterations
-        
-        # Runtime configuration
-        self.num_tasks = 2
-        self.num_tasks_per_node = 2  # Intra-node test
-        self.message_size = 8192
-        
-        # Performance reference
-        self.reference = {
-            'aion:cpu': {
-                'latency': (2.3, None, 0.1, 'µs')  # 2.3 µs ±10%
-            }
-        }
-        
-        # Sanity and performance patterns
-        self.sanity_patterns = sn.assert_found(
-            rf'^{self.message_size}\s+(\d+\.\d+)', 
-            self.stdout
-        )
-        self.perf_patterns = {
-            'latency': sn.extractsingle(
-                rf'^{self.message_size}\s+(\d+\.\d+)',
-                self.stdout, 1, float
-            )
-        }
-        
-        # NUMA binding
-        self.job_options = ['--bind-to numa']
 
+        self.num_tasks = 2
+        self.num_tasks_per_node = 2
+        self.reference = {
+            'aion_standalone:default': {'latency': (2.3, None, 0.1, 'µs')},
+            'aion:cpu': {'latency': (2.3, None, 0.1, 'µs')}
+        }
+        self.sanity_patterns = sn.assert_found(r'^8192\s+(\d+\.\d+)', self.stdout)
